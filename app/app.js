318643e850ee413ef7676c0837c34ecd7a3d90e1
slava hatnuke <slava.hatnuke@gmail.com>
2016-06-01 22:18:24 +0300

    - fixed nodemon runner

--- app/app.js
+++ app/app.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
 
+var neo4j = require('node-neo4j');
+var db = new neo4j('http://neo4j:test@neo4j:7474');
+
+//console.log('insert');
+//db.insertNode({
+//    name: 'Darth Vader',
+//    sex: 'male'
+//},function(err, node){
+//    if(err) throw err;
+//
+//    // Output node properties.
+//    console.log(node.data);
+//
+//    // Output node id.
+//    console.log(node._id);
+//});
+
 app.get('/', function(req, res){
-    res.send('hello world 1asdad');
+    res.send('hello world');
 });
 
 app.listen(3000, function () {
-    console.log('ok');
+    console.log('started');
 });
--- nodemon.sh
+++ nodemon.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose rm -f app && docker-compose up app
+docker-compose kill app && docker-compose rm -f app && docker-compose up app
