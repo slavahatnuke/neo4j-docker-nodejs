37e2ebd527f286fc25f3336076d89dc68441ae12
slava hatnuke <slava.hatnuke@gmail.com>
2016-06-10 20:04:49 +0300

    the simplest client for neo4j example (Angular/Bootstrap)

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@ var app = express();
 var neo4j = require('node-neo4j');
 var db = new neo4j('http://neo4j:test@neo4j:7474');
 
-app.get('/load', function (req, res, next) {
+app.use('/', express.static(__dirname + '/view'));
+
+app.get('/tools/load', function (req, res, next) {
     db.insertNode({
-        name: 'Darth Vader',
+        name: 'Darth Vader #' + parseInt(Math.random() * 100),
         sex: 'male'
     }, ['Person'], function (err, node) {
         if (err) return next(err);
@@ -15,19 +17,20 @@ app.get('/load', function (req, res, next) {
     });
 });
 
-app.get('/drop', function (req, res, next) {
+app.get('/tools/drop', function (req, res, next) {
     db.cypherQuery("MATCH (n) DETACH DELETE n", function (err, result) {
         if (err) return next(err);
         res.json(result);
     });
 });
 
-app.get('/', function (req, res, next) {
+app.get('/persons', function (req, res, next) {
     db.cypherQuery("MATCH (person:Person) RETURN person", function (err, result) {
         if (err) return next(err);
         res.json(result.data);
     });
 });
+
 
 app.listen(3000, function () {
     console.log('started');
